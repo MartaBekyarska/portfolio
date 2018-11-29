2d0f4f0d3c3a82bfd6bae727f4b66821191199cf
Marta Bekyarska <martatsoneva@yahoo.com>
2018-11-29 10:28:46 +0000

    Updates

---
+++ precache-manifest.36aac7337dd449b64c666071569bcd5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "8b0d42a9ec734746fd02",
+    "url": "/portfolio/static/js/main.8b0d42a9.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/portfolio/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "8b0d42a9ec734746fd02",
+    "url": "/portfolio/static/css/main.97a5e277.chunk.css"
+  },
+  {
+    "revision": "6d1e711691e625ede991d7e546a7e721",
+    "url": "/portfolio/index.html"
+  }
+];
