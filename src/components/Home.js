37a3e5ea9cc53d058fc9744b54a1d478b18e2d04
Marta Bekyarska <martatsoneva@yahoo.com>
2018-11-29 12:17:54 +0000

    New background image added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import SpiralSteps from '../images/spiralsteps.jpg';
+import unsplash from '../images/unsplash.jpg';
 import '../css/home.css';
 
 const Home = () => {
   return (
         <div className="message">
           <div className="wrapImage">
-            <img src={SpiralSteps} alt="spiral-steps" />
+            <img src={unsplash} alt="spiral-steps" />
           </div>
           <div className="wrapText">
             <h1>Welcome! My name is Marta. I'm a Front End developer working with JavaScript and React with experience in testing.</h1>
