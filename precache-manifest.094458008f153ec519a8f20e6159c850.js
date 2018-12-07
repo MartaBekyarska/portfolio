6198a7beb9a57f53fb5494cdacdadef3ce5df324
Marta Bekyarska <martatsoneva@yahoo.com>
2018-12-07 15:29:43 +0000

    Updates

---
+++ precache-manifest.094458008f153ec519a8f20e6159c850.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9500001262f85b7cbed93f18d417186a",
+    "url": "/portfolio/static/media/marta.95000012.jpg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "902d5a340593049a5e1e",
+    "url": "/portfolio/static/js/main.902d5a34.chunk.js"
+  },
+  {
+    "revision": "26d580dd159b8863084f",
+    "url": "/portfolio/static/js/1.26d580dd.chunk.js"
+  },
+  {
+    "revision": "902d5a340593049a5e1e",
+    "url": "/portfolio/static/css/main.58331027.chunk.css"
+  },
+  {
+    "revision": "4d053609f99b212399ec5c333daa506c",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.57a5b6a01d35e46d33eb0db3c17bf4ff.js"
+  "/portfolio/precache-manifest.094458008f153ec519a8f20e6159c850.js"
 );
 
 workbox.clientsClaim();
