6f08e9c4693a56925c22131378b99d1b524073b5
Marta Bekyarska <martatsoneva@yahoo.com>
2018-12-11 09:40:29 +0000

    Added underlining to the nav buttons on hover

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import './contact.css';
+import './Contact.css';
 import Menu1 from '../Menu1/Menu1';
 
 const Contact = () => {
     return (
         <div class="container">
             <Menu1 />
+            <h1 className="message">If you wish to contact me via email please click <a class="mailto" href="mailto:martatsoneva@yahoo.com">here </a>or simply send me a message through <a href="https://www.linkedin.com/in/marta-bekyarska-4280b7137/">Linkedin</a>.</h1>
         </div>
     );
 };
